8b6f896dfa29c345532aadae6ee3ca5565ed7a42
Michaelangel007 <testcraft@sharedcraft.com>
2015-06-07 13:52:27 -0700

    Fixed verification

--- verify.sh
+++ verify.sh
@@ -2,13 +2,11 @@
 
 mkdir -p data
 cd data
+
 ../bin/buddhabrot
-
 ../bin/omp
-
-diff "cpu1_raw_buddhabrot_1024x768_1000_10x.u16.data" "omp1_raw_buddhabrot_1024x768_1000_10x.u16.data"
-
 ../bin/omp2
 
-diff "cpu1_raw_buddhabrot_1024x768_1000_10x.u16.data" "omp2_raw_buddhabrot_1024x768_1000_10x.u16.data"
+diff "cpu1_buddhabrot_1024x768_1000_10x.u16.data" "omp1_buddhabrot_1024x768_1000_10x.u16.data"
+diff "cpu1_buddhabrot_1024x768_1000_10x.u16.data" "omp2_buddhabrot_1024x768_1000_10x.u16.data"
 
