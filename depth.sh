1a57fff54f17192d5f20a0328a48c172fd5154d3
Michaelangel007 <testcraft@sharedcraft.com>
2015-06-07 20:46:28 -0700

    Clarified depth.sh

--- depth.sh
+++ depth.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+mkdir -p depth
+cd       depth
+
+echo -e "\nDepth 4000"
+
 ../bin/omp2 -v 4000 3000 4000
 ../bin/omp2    4000 3000 4000
+
+echo -e "\nDepth 2000"
 
 ../bin/omp2 -v 4000 3000 2000
 ../bin/omp2    4000 3000 2000
 
+echo -e "\nDepth 1000"
+
 ../bin/omp2 -v 4000 3000 1000
 ../bin/omp2    4000 3000 1000
+
+
