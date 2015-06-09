491ebab9a6e5b0a56475917e66b2abeef60c6b53
Michaelangel007 <testcraft@sharedcraft.com>
2015-06-09 14:24:43 -0700

    Updated depth to use omp3

--- depth.sh
+++ depth.sh
@@ -5,17 +5,16 @@ cd       depth
 
 echo -e "\nDepth 4000"
 
-../bin/omp2 -v 4000 3000 4000
-../bin/omp2    4000 3000 4000
+../bin/omp3 -v 4000 3000 4000
+../bin/omp3    4000 3000 4000
 
 echo -e "\nDepth 2000"
 
-../bin/omp2 -v 4000 3000 2000
-../bin/omp2    4000 3000 2000
+../bin/omp3 -v 4000 3000 2000
+../bin/omp3    4000 3000 2000
 
 echo -e "\nDepth 1000"
 
-../bin/omp2 -v 4000 3000 1000
-../bin/omp2    4000 3000 1000
-
+../bin/omp3 -v 4000 3000 1000
+../bin/omp3    4000 3000 1000
 
