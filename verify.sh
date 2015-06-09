8bfd33db34c2c50b4f3c3129ac4a15e74fbb2dd9
Michaelangel007 <testcraft@sharedcraft.com>
2015-06-09 11:54:41 -0700

    Fixed description comparing images

--- verify.sh
+++ verify.sh
@@ -9,7 +9,7 @@ echo -e "\nMulti core v2 ..." ; ../bin/omp2
 echo -e "\nMulti core v3 ..." ; ../bin/omp3
 echo -e "\nMulti core v4 ..." ; ../bin/omp4
 
-echo -e "\nComparing raw bitmaps ..."
+echo -e "\nComparing raw images ..."
 
 diff "raw_cpu1_buddhabrot_1024x768_1000_10x.u16.data" "raw_omp1_buddhabrot_1024x768_1000_10x.u16.data"
 diff "raw_cpu1_buddhabrot_1024x768_1000_10x.u16.data" "raw_omp2_buddhabrot_1024x768_1000_10x.u16.data"
