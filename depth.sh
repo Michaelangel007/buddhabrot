039a0f7a9dc950c158b060b5cdc77abdcb618650
Michaelangel007 <testcraft@sharedcraft.com>
2015-06-10 09:49:01 -0700

    Fix to not display -e

--- depth.sh
+++ depth.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p depth
 cd       depth
 
-echo -e "\nDepth 4000"
+echo "\nDepth 4000"
 
 ../bin/omp3 -v 4000 3000 4000
 ../bin/omp3    4000 3000 4000
 
-echo -e "\nDepth 2000"
+echo "\nDepth 2000"
 
 ../bin/omp3 -v 4000 3000 2000
 ../bin/omp3    4000 3000 2000
 
-echo -e "\nDepth 1000"
+echo "\nDepth 1000"
 
 ../bin/omp3 -v 4000 3000 1000
 ../bin/omp3    4000 3000 1000
