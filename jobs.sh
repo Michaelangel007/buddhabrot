e75c2025040acb6eafaa55645bda4eb2ded14858
Michaelangel007 <testcraft@sharedcraft.com>
2015-06-10 15:12:31 -0700

    Updated jobs.sh to time up to 8 threads

--- jobs.sh
+++ jobs.sh
@@ -3,10 +3,14 @@
 mkdir -p jobs
 cd       jobs
 
-../bin/omp3 -j1 ; echo "" ; mv raw_omp3_buddhabrot_1024x768_1000_10x.u16.data 1.data ; mv omp3_buddhabrot_768x1024_1000.bmp 1.bmp
-../bin/omp3 -j2 ; echo "" ; mv raw_omp3_buddhabrot_1024x768_1000_10x.u16.data 2.data ; mv omp3_buddhabrot_768x1024_1000.bmp 2.bmp
-../bin/omp3 -j3 ; echo "" ; mv raw_omp3_buddhabrot_1024x768_1000_10x.u16.data 3.data ; mv omp3_buddhabrot_768x1024_1000.bmp 3.bmp
-../bin/omp3 -j4 ; echo "" ; mv raw_omp3_buddhabrot_1024x768_1000_10x.u16.data 4.data ; mv omp3_buddhabrot_768x1024_1000.bmp 4.bmp
+../bin/omp4 -j1 ; echo "" ; mv raw_omp4_buddhabrot_1024x768_1000_10x.u16.data 1.data ; mv omp4_buddhabrot_768x1024_1000.bmp 1.bmp
+../bin/omp4 -j2 ; echo "" ; mv raw_omp4_buddhabrot_1024x768_1000_10x.u16.data 2.data ; mv omp4_buddhabrot_768x1024_1000.bmp 2.bmp
+../bin/omp4 -j3 ; echo "" ; mv raw_omp4_buddhabrot_1024x768_1000_10x.u16.data 3.data ; mv omp4_buddhabrot_768x1024_1000.bmp 3.bmp
+../bin/omp4 -j4 ; echo "" ; mv raw_omp4_buddhabrot_1024x768_1000_10x.u16.data 4.data ; mv omp4_buddhabrot_768x1024_1000.bmp 4.bmp
+../bin/omp4 -j5 ; echo "" ; mv raw_omp4_buddhabrot_1024x768_1000_10x.u16.data 5.data ; mv omp4_buddhabrot_768x1024_1000.bmp 5.bmp
+../bin/omp4 -j6 ; echo "" ; mv raw_omp4_buddhabrot_1024x768_1000_10x.u16.data 6.data ; mv omp4_buddhabrot_768x1024_1000.bmp 6.bmp
+../bin/omp4 -j7 ; echo "" ; mv raw_omp4_buddhabrot_1024x768_1000_10x.u16.data 7.data ; mv omp4_buddhabrot_768x1024_1000.bmp 7.bmp
+../bin/omp4 -j8 ; echo "" ; mv raw_omp4_buddhabrot_1024x768_1000_10x.u16.data 8.data ; mv omp4_buddhabrot_768x1024_1000.bmp 8.bmp
 
 echo "Comparing raw images..."
 diff 1.data 2.data
