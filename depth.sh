882c92cf52032ff252288a46370c9fc8ba5e5aa6
Michaelangel007 <testcraft@sharedcraft.com>
2015-06-11 00:00:38 -0700

    Add newlines, depth 3000

--- depth.sh
+++ depth.sh
@@ -3,18 +3,19 @@
 mkdir -p depth
 cd       depth
 
-echo "\nDepth 4000"
-
+echo -e "\nDepth 4000"
 ../bin/omp3 -v 4000 3000 4000
 ../bin/omp3    4000 3000 4000
 
-echo "\nDepth 2000"
+echo -e "\nDepth 3000"
+../bin/omp3 -v 4000 3000 3000
+../bin/omp3    4000 3000 3000
 
+echo -e "\nDepth 2000"
 ../bin/omp3 -v 4000 3000 2000
 ../bin/omp3    4000 3000 2000
 
-echo "\nDepth 1000"
-
+echo -e "\nDepth 1000"
 ../bin/omp3 -v 4000 3000 1000
 ../bin/omp3    4000 3000 1000
 
